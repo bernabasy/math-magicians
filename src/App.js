59dfc1d9e5034fce2510f5180b2cf2931c80fd08
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-01-17 18:51:28 +0300

    Develop just the UI of the calculator with few css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
 export default App;
