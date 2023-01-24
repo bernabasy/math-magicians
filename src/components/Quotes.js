af7768504b6c908a9ceedfb50a731170b4b04e1c
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-01-24 16:37:38 +0300

    Add Quotes.js file

---
+++ src/components/Quotes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Quotes() {
+  return (
+    <h2 className="quotes">
+      Mathematices is not about numbers equtions or algorithms: it is about
+      undertanding William Paoul Thurston
+    </h2>
+  );
+}
